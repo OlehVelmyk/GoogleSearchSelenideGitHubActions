ed8aa3e2e823fd866bc9737634fb460e59c72ac8
Oleh Velmyk <oleh.velmyk@gmail.com>
2024-04-30 20:57:39 +0300

    1. Updated jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -34,7 +34,6 @@ node {
                        generateAllure()
                    }
                 }
-            }
 
 //        try {
 //            parallel getTestStages(["apiTests", "uiTests"])
