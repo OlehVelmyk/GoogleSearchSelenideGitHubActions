cdb5d35c6c1e72c6a552aedfe9202bf80c88cb33
Oleh Velmyk <oleh.velmyk@gmail.com>
2024-05-01 20:41:15 +0300

    1. Updated jenkinsnew.groovy

--- jenkinsnew.groovy
+++ jenkinsnew.groovy
@@ -34,7 +34,7 @@ node {
                 stage("Run tests in ${browser_name}") {
 //                    labelledShell(label: "Run ${browser_name}", script: "mvn clean test -DbrowserName=${browser_name}")
                     label name: "Run ${browser_name}"
-                    bat "mvn clean test -DbrowserName=${browser_name}"
+                    bat(label: "Run ${browser_name}", script: "mvn clean test -DbrowserName=${browser_name}")
                 }
             } catch (err) {
                     echo "Some failed tests ${browser_name}"
