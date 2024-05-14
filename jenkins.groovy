b2839ba3009d1290422c228774f7bff517f5c678
Oleh Velmyk <oleh.velmyk@gmail.com>
2024-05-14 17:44:59 +0300

    Updated jenkins.groovy

--- jenkins.groovy
+++ jenkins.groovy
@@ -114,6 +114,7 @@ def generateAllure() {
 def sendSlackNotification() {
     slackSend botUser: true,
               channel: 'test_notifications',
+              iconEmoji: ':jenkins:',
               color: '#00ff00',
               message: "Jenkins <$env.JOB_BASE_NAME> completed!!! $currentBuild.result \r\n" +
                        "Branch: $task_branch. Browser: $browser_name. \r\n" +
